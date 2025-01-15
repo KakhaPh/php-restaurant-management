2ef5c5c16157654920aee0851bcf4f03d15b43ad
KakhaPh <kakha.putkaradze@eu.edu.ge>
2025-01-15 17:57:10 +0400

    App.php for content

---
+++ libs/App.php
@@ -0,0 +1,14 @@
+<?php require_once  "../config/config.php" ?>
+
+<?php
+
+    class App {
+        public $host = DB_HOST;
+        public $dbname = DB_NAME;
+        public $username = DB_USERNAME;
+        public $password = DB_PASSWORD;
+
+        public $link;
+
+        // 
+    }
