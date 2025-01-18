929a0dc8bd0a869233cd07a1b384bc546b2279b2
KakhaPh <kakha.putkaradze@eu.edu.ge>
2025-01-18 16:07:21 +0400

    header.php modify for routing between pages

--- includes/header.php
+++ includes/header.php
@@ -43,18 +43,18 @@ define("baseUrl", "/");
 <body>
     <div class="container-xxl bg-white p-0">
         <!-- Spinner Start -->
-        <!-- <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+        <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
             <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
                 <span class="sr-only">Loading...</span>
             </div>
-        </div> -->
+        </div>
         <!-- Spinner End -->
 
 
         <!-- Navbar & Hero Start -->
         <div class="container-xxl position-relative p-0">
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-4 px-lg-5 py-3 py-lg-0">
-                <a href="" class="navbar-brand p-0">
+                <a href="<?php echo baseUrl; ?>" class="navbar-brand p-0">
                     <h1 class="text-primary m-0"><i class="fa fa-utensils me-3"></i>Restaurant</h1>
                     <!-- <img src="img/logo.png" alt="Logo"> -->
                 </a>
@@ -63,19 +63,19 @@ define("baseUrl", "/");
                 </button>
                 <div class="collapse navbar-collapse" id="navbarCollapse">
                     <div class="navbar-nav ms-auto py-0 pe-4">
-                        <a href="index.html" class="nav-item nav-link active">Home</a>
-                        <a href="about.html" class="nav-item nav-link">About</a>
-                        <a href="service.html" class="nav-item nav-link">Service</a>
-                        <a href="menu.html" class="nav-item nav-link">Menu</a>
+                        <a href="<?php echo baseUrl; ?>" class="nav-item nav-link active">Home</a>
+                        <a href="<?php echo baseUrl . "about.php"; ?>" class="nav-item nav-link">About</a>
+                        <a href="<?php echo baseUrl . "service.php"; ?>" class="nav-item nav-link">Service</a>
+                        <a href="<?php echo baseUrl . "menu.php"; ?>" class="nav-item nav-link">Menu</a>
                         <div class="nav-item dropdown">
                             <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
                             <div class="dropdown-menu m-0">
-                                <a href="booking.html" class="dropdown-item">Booking</a>
-                                <a href="team.html" class="dropdown-item">Our Team</a>
-                                <a href="testimonial.html" class="dropdown-item">Testimonial</a>
+                                <a href="<?php echo baseUrl . "booking.php"; ?>" class="dropdown-item">Booking</a>
+                                <a href="<?php echo baseUrl . "team.php"; ?>" class="dropdown-item">Our Team</a>
+                                <a href="<?php echo baseUrl . "testimonial.php"; ?>" class="dropdown-item">Testimonial</a>
                             </div>
                         </div>
-                        <a href="contact.html" class="nav-item nav-link">Contact</a>
+                        <a href="<?php echo baseUrl . "contact.php"; ?>" class="nav-item nav-link">Contact</a>
                         <?php if (isset($_SESSION['user_id'])) : ?>
                             <div class="nav-item dropdown">
                                 <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><?php echo htmlspecialchars($_SESSION['username'], ENT_QUOTES, 'UTF-8'); ?></a>
